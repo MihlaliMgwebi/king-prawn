8184b8f401ca06de98690d29e8add45c0a9c1e81
Sechaba Mache <93662067+sechaba-mache@users.noreply.github.com>
2024-07-20 15:00:01 +0200

    Search stuff

--- src/app/components/search-modal/search-modal.component.ts
+++ src/app/components/search-modal/search-modal.component.ts
@@ -1,23 +1,41 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ShoppingResultService } from '../../api/shopping-result/shopping-result.service';
+import { SearchCardComponent } from "../search-card/search-card.component";
 
 @Component({
   selector: 'app-search-modal',
   standalone: true,
-  imports: [ CommonModule ],
+  imports: [CommonModule, SearchCardComponent],
   templateUrl: './search-modal.component.html',
   styleUrl: './search-modal.component.scss'
 })
 export class SearchModalComponent {
   enableImageSearch = false;
   searchText = "";
+  cards: any[] = [];
 
   constructor(private searchService: ShoppingResultService) {}
 
   search(searchText: string) {
-    this.searchService.search(searchText).subscribe((data) => {
-      console.log(data);
+    console.log(searchText);
+    
+    this.searchService.search(searchText).subscribe((data: any) => {
+      data.filtered_results.map((card: any) => {
+        this.cards.push({
+          product: card.title,
+          vendor: card.source,
+          price: card.price,
+          imgUrl: card.thumbnail
+        })
+      })
+      console.log(this.cards)
     })
+
+  }
+
+  updateSearchText(text: any) {
+    this.searchText = text.target.value;
+    console.log(this.searchText)
   }
 }
