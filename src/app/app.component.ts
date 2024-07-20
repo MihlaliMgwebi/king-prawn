4d0984f4d06d10bea8da5cce9ce379bb4185d989
Kivesh Virasamy <kvirasamy@jhb.dvt.co.za>
2024-07-20 13:11:36 +0200

    feat: :sparkles: Backend intergration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AccountService } from './api/account/account.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'king-prawn';
+  private _accountService = inject(AccountService);
+  constructor() { 
+    console.log(this._accountService.getList().subscribe(x => console.log(x)));
+  }
+
 }
