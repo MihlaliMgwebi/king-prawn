176eb6fd103e5a54261a655cc437eec785b385e5
Sechaba Mache <93662067+sechaba-mache@users.noreply.github.com>
2024-07-20 12:00:27 +0200

    Create search cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ const { default: daisyui } = require('daisyui');
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'card': '#D9D9D9',
+        'primary-btn': '#0E0047'
+      } ,
+      textColor: {
+        'grey': '#9F9F9F',
+        'primary': '#3D4451'
+      },
+      boxShadow: {
+        'search-card': '0px 2px 4px 0px rgba(0, 0, 0, 0.25);'
+      }
+    },
   },
   plugins: [require('daisyui')],
 }
