0305f0849ffddc323be35681b03d4b72b50871e1
kphutiyagae <kopanophutiyagae@gmail.com>
2024-07-20 11:07:11 +0200

    feat(none): Added components to routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import {WishlistComponent} from "./pages/wishlist/wishlist.component";
+import {AccountsComponent} from "./pages/accounts/accounts.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'wishlist',
+    component: WishlistComponent
+  },
+  {
+    path: 'accounts',
+    component: AccountsComponent
+  },
+  {
+    path: '',
+    redirectTo: '/accounts',
+    pathMatch: 'full'
+  }
+];
